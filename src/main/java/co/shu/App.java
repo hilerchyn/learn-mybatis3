7fd590e0eeb43a7f9a3a7aebd7a547bcbb0fecd1
chen.tao <hilerchyn@gmail.com>
2024-01-17 21:15:26 +0800

    init

---
+++ src/main/java/co/shu/App.java
@@ -0,0 +1,14 @@
+package co.shu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.exit(0);
+    }
+}
