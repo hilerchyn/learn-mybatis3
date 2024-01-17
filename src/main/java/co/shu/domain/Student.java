5b82b8c5f89283e06737931262f9fc6c1f1d2850
chen.tao <hilerchyn@gmail.com>
2024-01-17 22:32:47 +0800

    implements student class

---
+++ src/main/java/co/shu/domain/Student.java
@@ -0,0 +1,12 @@
+package co.shu.domain;
+
+import java.util.Date;
+
+public class Student {
+    private Integer studId;
+    private String  name;
+    private String  email;
+    private Date    dob;
+
+    // FIXME: implements setters and getters
+}
