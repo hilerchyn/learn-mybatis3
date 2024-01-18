04a0e98c8d5d121a4df93b86775fb861a718cd64
chen.tao <hilerchyn@gmail.com>
2024-01-18 08:51:00 +0800

    fix setters and getters of domain Student

--- src/main/java/co/shu/domain/Student.java
+++ src/main/java/co/shu/domain/Student.java
@@ -8,5 +8,35 @@ public class Student {
     private String  email;
     private Date    dob;
 
-    // FIXME: implements setters and getters
+    public Integer getStudId(){
+        return studId;
+    }
+
+    public void setStudId(Integer studId) {
+        this.studId = studId;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
 }
