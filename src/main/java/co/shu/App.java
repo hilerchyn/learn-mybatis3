773656763934cd8f6eff3016fbc5cbe8f805ab59
chen.tao <hilerchyn@gmail.com>
2024-01-18 13:44:53 +0800

    create student

--- src/main/java/co/shu/App.java
+++ src/main/java/co/shu/App.java
@@ -3,6 +3,8 @@ package co.shu;
 import co.shu.services.StudentService;
 import java.util.List;
 import co.shu.domain.Student;
+import java.time.Instant;
+import java.util.Date;
 
 public class App
 {
@@ -22,5 +24,19 @@ public class App
         // get one student
         Student student = studentService.findStudentById(2);
         System.out.printf("%d, %s, %s, %s\n", student.getStudId(), student.getName(), student.getEmail(), student.getDob().toString());
+
+        // create student
+        student = new Student();
+        Long id = Instant.now().getEpochSecond();
+        String idStr = String.format("%d", Instant.now().getEpochSecond());
+        student.setStudId(id.intValue());
+        student.setName("student_" + idStr);
+        student.setEmail("student_" + idStr + "@gmail.com");
+        student.setDob(new Date());
+        studentService.createStudent(student);
+
+        // fetch the created student
+        student = studentService.findStudentById(id.intValue());
+        System.out.printf("created student: %d, %s, %s, %s\n", student.getStudId(), student.getName(), student.getEmail(), student.getDob().toString());
     }
 }
